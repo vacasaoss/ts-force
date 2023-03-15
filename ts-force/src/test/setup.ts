3417693d60aa44e20cfd65abb98624f6054e795c
Jack Holmes <jack.holmes@vacasa.com>
2023-03-15 14:45:59 +1300

    Use vacasaoss org

--- ts-force/src/test/setup.ts
+++ ts-force/src/test/setup.ts
@@ -1,21 +1,24 @@
-import * as cprocess from 'child_process';
-import * as fs from 'fs';
+import * as cprocess from "child_process";
+import * as fs from "fs";
 
-require('dotenv').config();
+require("dotenv").config();
 genClasses();
 
-function genClasses () {
-    cprocess.exec(`ts-node ../ts-force-gen/src/index -e -j ./src/test/assets/ts-force-config.json`, (err, data) => {
+function genClasses() {
+  cprocess.exec(
+    `ts-node ../ts-force-gen/src/index -e -j ./src/test/assets/ts-force-config.json`,
+    (err, data) => {
+      if (err) {
+        console.log(err, data);
+        process.exit(1);
+      }
+      let result = data.replace(/from "@vacasaoss\/ts-force"/g, `from '../..'`);
+      fs.writeFile("./src/test/assets/sobs.ts", result, (err) => {
         if (err) {
-            console.log(err, data);
-            process.exit(1);
+          console.log(err);
+          process.exit(1);
         }
-        let result = data.replace(/from "ts-force"/g, `from '../..'`);
-        fs.writeFile('./src/test/assets/sobs.ts', result, (err) => {
-            if (err) {
-                console.log(err);
-                process.exit(1);
-            }
-        });
-    });
+      });
+    }
+  );
 }
